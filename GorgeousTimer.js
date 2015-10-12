adb10adc9fdd59c1cb7601dba354c9b12c165a5b
buriburipro <buriburipro@outlook.com>
2015-10-12 22:04:53 +0800

    Revert "change"
    This reverts commit 61fcf05d35d64e37d78e58d1ccde7d73047a5d16.

--- GorgeousTimer.js
+++ GorgeousTimer.js
@@ -18,7 +18,8 @@ window.onload = function(){
 
 function getCurTime(){
 	var tempTime = new Date(),
-		ret = endTime.getTime() - tempTime.getTime();
-	ret = Math.round(ret / 1000);
-	return (ret > 0)? ret : 0;
+		rest = endTime.getTime() - tempTime.getTime();
+	rest = Math.round(ret / 1000);
+	return (rest > 0)? rest : 0;
 }
+
