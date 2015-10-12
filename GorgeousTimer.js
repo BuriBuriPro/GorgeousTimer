fa7dc82c8dac7d9bb94825ef424047d71eb5fe2e
buriburipro <buriburipro@outlook.com>
2015-10-12 23:24:28 +0800

    show the number of static timer

--- GorgeousTimer.js
+++ GorgeousTimer.js
@@ -1,8 +1,9 @@
 var windowWidth = 1024,
-	windowHeight = 768,
+	windowHeight = 600,
 	marginTop = 60,
 	marginLeft = 30,
 	radius = 8;
+const pi = Math.PI;
 
 var endTime = new Date(2015, 9, 13, 0, 0, 0),
 	curTime = 0;
@@ -13,12 +14,48 @@ window.onload = function(){
 	canvas.width = windowWidth;
 	canvas.height = windowHeight;
 	curTime = getCurTime();
-	// console.log(curTime);
+	
+	render(context);
 }
 
 function getCurTime(){
 	var tempTime = new Date(),
 		rest = endTime.getTime() - tempTime.getTime();
-	rest = Math.round(ret / 1000);
+	rest = Math.round(rest / 1000);
 	return (rest > 0)? rest : 0;
+}
+function render(cxt){
+	// get the numbers of hour, minute, second
+	// var hour = parseInt(curTime / 3600),
+	// 	minute = parseInt((curTime - hour * 3600) / 60),
+	// 	second = curTime % 60;
+	var hour = 12,
+		minute = 34,
+		second = 56;
+	// hour
+	renderNumber(marginLeft, marginTop, parseInt(hour / 10), cxt);
+	renderNumber(marginLeft + 15 * (radius + 1), marginTop, parseInt(hour % 10), cxt);
+	// colon
+	renderNumber(marginLeft + 30 * (radius + 1), marginTop, 10, cxt);
+	// minute
+	renderNumber(marginLeft + 39 * (radius + 1), marginTop, parseInt(minute / 10), cxt);
+	renderNumber(marginLeft + 54 * (radius + 1), marginTop, parseInt(minute % 10), cxt);
+	// colon
+	renderNumber(marginLeft + 69 * (radius + 1), marginTop, 10, cxt);
+	// second
+	renderNumber(marginLeft + 78 * (radius + 1), marginTop, parseInt(second / 10), cxt);
+	renderNumber(marginLeft + 93 * (radius + 1), marginTop, parseInt(second % 10), cxt);
+}
+function renderNumber(x, y, num, cxt){
+	var i, j;
+	cxt.fillStyle = "blue";
+	for(i = 0; i < digit[num].length; i ++)
+		for(j = 0; j < digit[num][i].length; j ++){
+			if(digit[num][i][j] === 1){
+				cxt.beginPath();
+				cxt.arc(x + j * 2 * (radius + 1) + radius + 1, y + i * 2 * (radius + 1) + radius + 1, radius, 0, 2 * pi);
+				cxt.fill();
+				cxt.closePath();
+			}
+		}	
 }
