f8880439eb4e410d0e53a8726e6267edecc40452
buriburipro <buriburipro@outlook.com>
2015-10-12 22:05:02 +0800

    Revert "add function to get current time"
    This reverts commit 469b29e069b1319bb24bd10c9314f8930338a8e0.

--- GorgeousTimer.js
+++ GorgeousTimer.js
@@ -3,22 +3,3 @@ var windowWidth = 1024,
 	marginTop = 60,
 	marginLeft = 30,
 	radius = 8;
-
-var endTime = new Date(2015, 9, 13, 0, 0, 0),
-	curTime = 0;
-
-window.onload = function(){
-	var canvas = document.getElementById("canvas"),
-		context = canvas.getContext("2d");
-	canvas.width = windowWidth;
-	canvas.height = windowHeight;
-	curTime = getCurTime();
-	// console.log(curTime);
-}
-
-function getCurTime(){
-	var tempTime = new Date(),
-		rest = endTime.getTime() - tempTime.getTime();
-	rest = Math.round(ret / 1000);
-	return (rest > 0)? rest : 0;
-}
